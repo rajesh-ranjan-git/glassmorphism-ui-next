68430e7b0fdf463e7b83d6388f18c0cdc489aa4c
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-10-22 13:55:15 +0530

    feature/animatedBackground

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Main from "@/components/main/main";
+import AnimatedBackground from "@/components/background/animatedBackground";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen font-alkatra text-5xl">
-      Glassmorphism UI
-    </div>
+    <>
+      <AnimatedBackground />
+      <Main />
+    </>
   );
 }
---
+++ src/components/main/main.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <div className="flex justify-center items-center bg-black w-screen h-screen font-alkatra text-white text-5xl underline underline-offset-8">
+      Glassmorphism UI
+    </div>
+  );
+};
+
+export default Main;
