276714248c587258bc28efcba4ed90b94624f1da
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-10-23 08:03:08 +0530

    animated login page

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # Glassmorphism UI - Next JS
 
-## Lets build glassmorphism themed components using Next JS!
-
-- [x] Google Fonts available with Next JS
-- [x] Animated Background
-- [x] Animated Glowing Buttons
-- [x] User Card
-- [ ] Login Page
-- [ ] Social Media Icons
-- [ ] Animated Theme Toggler Button
+## Animated Login Page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,74 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen font-alkatra text-5xl">
-      Glassmorphism UI
-    </div>
+    <section>
+      <div className="color"></div>
+      <div className="color"></div>
+      <div className="color"></div>
+
+      <div className="box">
+        <div
+          className="square"
+          style={
+            {
+              "--i": 0,
+            } as React.CSSProperties
+          }
+        ></div>
+        <div
+          className="square"
+          style={
+            {
+              "--i": 1,
+            } as React.CSSProperties
+          }
+        ></div>
+        <div
+          className="square"
+          style={
+            {
+              "--i": 2,
+            } as React.CSSProperties
+          }
+        ></div>
+        <div
+          className="square"
+          style={
+            {
+              "--i": 3,
+            } as React.CSSProperties
+          }
+        ></div>
+        <div
+          className="square"
+          style={
+            {
+              "--i": 4,
+            } as React.CSSProperties
+          }
+        ></div>
+        <div className="container">
+          <div className="form">
+            <h2>Login Form</h2>
+            <form>
+              <div className="inputBox">
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="inputBox">
+                <input type="password" placeholder="Password" />
+              </div>
+              <div className="inputBox">
+                <input type="submit" value="Login" />
+              </div>
+              <p className="forget">
+                Forgot password? <a href="#">Click Here</a>
+              </p>
+              <p className="forget">
+                Don't have an account? <a href="#">Sign Up</a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
