c2dccf109a6885f06dd15747308430a3dfe6f903
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-10-23 15:36:22 +0530

    working till now

--- src/components/socialMedia/socialMedia.tsx
+++ src/components/socialMedia/socialMedia.tsx
@@ -12,7 +12,7 @@ const SocialMedia = () => {
       <li className="relative list-none">
         <a
           href="#"
-          className="top-0 before:top-0 before:left-0 before:absolute relative flex justify-center items-center before:bg-white/50 shadow-black/10 shadow-lg backdrop-blur-xs border border-white/40 border-r-white/20 border-b-white/20 rounded-xl w-20 before:w-12 h-20 before:h-full overflow-hidden text-white text-4xl no-underline before:content-[''] before:skew-x-45 before:transition before:translate-x-36 hover:before:-translate-x-36 before:duration-500"
+          className="top-0 before:top-0 hover:-top-12 before:left-0 z-1 hover:z-11 before:absolute relative flex justify-center items-center before:bg-white/50 shadow-black/10 shadow-lg backdrop-blur-xs border border-white/40 border-r-white/20 border-b-white/20 rounded-xl w-20 before:w-12 h-20 before:h-full overflow-hidden text-white text-4xl no-underline before:content-[''] before:skew-x-45 before:transition before:translate-x-36 hover:before:-translate-x-36 before:duration-500"
         >
           <FaFacebookF />
         </a>
@@ -20,7 +20,7 @@ const SocialMedia = () => {
       <li className="relative list-none">
         <a
           href="#"
-          className="top-0 before:top-0 before:left-0 before:absolute relative flex justify-center items-center before:bg-white/50 shadow-black/10 shadow-lg backdrop-blur-xs border border-white/40 border-r-white/20 border-b-white/20 rounded-xl w-20 before:w-12 h-20 before:h-full overflow-hidden text-white text-4xl no-underline before:content-[''] before:skew-x-45 before:transition before:translate-x-36 hover:before:-translate-x-36 before:duration-500"
+          className="top-0 before:top-0 hover:-top-12 before:left-0 z-1 hover:z-11 before:absolute relative flex justify-center items-center before:bg-white/50 shadow-black/10 shadow-lg backdrop-blur-xs border border-white/40 border-r-white/20 border-b-white/20 rounded-xl w-20 before:w-12 h-20 before:h-full overflow-hidden text-white text-4xl no-underline before:content-[''] before:skew-x-45 before:transition before:translate-x-36 hover:before:-translate-x-36 before:duration-500"
         >
           <FaInstagram />
         </a>
@@ -28,7 +28,7 @@ const SocialMedia = () => {
       <li className="relative list-none">
         <a
           href="#"
-          className="top-0 before:top-0 before:left-0 before:absolute relative flex justify-center items-center before:bg-white/50 shadow-black/10 shadow-lg backdrop-blur-xs border border-white/40 border-r-white/20 border-b-white/20 rounded-xl w-20 before:w-12 h-20 before:h-full overflow-hidden text-white text-4xl no-underline before:content-[''] before:skew-x-45 before:transition before:translate-x-36 hover:before:-translate-x-36 before:duration-500"
+          className="top-0 before:top-0 hover:-top-12 before:left-0 z-1 hover:z-11 before:absolute relative flex justify-center items-center before:bg-white/50 shadow-black/10 shadow-lg backdrop-blur-xs border border-white/40 border-r-white/20 border-b-white/20 rounded-xl w-20 before:w-12 h-20 before:h-full overflow-hidden text-white text-4xl no-underline before:content-[''] before:skew-x-45 before:transition before:translate-x-36 hover:before:-translate-x-36 before:duration-500"
         >
           <FaXTwitter />
         </a>
@@ -36,7 +36,7 @@ const SocialMedia = () => {
       <li className="relative list-none">
         <a
           href="#"
-          className="top-0 before:top-0 before:left-0 before:absolute relative flex justify-center items-center before:bg-white/50 shadow-black/10 shadow-lg backdrop-blur-xs border border-white/40 border-r-white/20 border-b-white/20 rounded-xl w-20 before:w-12 h-20 before:h-full overflow-hidden text-white text-4xl no-underline before:content-[''] before:skew-x-45 before:transition before:translate-x-36 hover:before:-translate-x-36 before:duration-500"
+          className="top-0 before:top-0 hover:-top-12 before:left-0 z-1 hover:z-11 before:absolute relative flex justify-center items-center before:bg-white/50 shadow-black/10 shadow-lg backdrop-blur-xs border border-white/40 border-r-white/20 border-b-white/20 rounded-xl w-20 before:w-12 h-20 before:h-full overflow-hidden text-white text-4xl no-underline before:content-[''] before:skew-x-45 before:transition before:translate-x-36 hover:before:-translate-x-36 before:duration-500"
         >
           <FaLinkedinIn />
         </a>
@@ -44,7 +44,7 @@ const SocialMedia = () => {
       <li className="relative list-none">
         <a
           href="#"
-          className="top-0 before:top-0 before:left-0 before:absolute relative flex justify-center items-center before:bg-white/50 shadow-black/10 shadow-lg backdrop-blur-xs border border-white/40 border-r-white/20 border-b-white/20 rounded-xl w-20 before:w-12 h-20 before:h-full overflow-hidden text-white text-4xl no-underline before:content-[''] before:skew-x-45 before:transition before:translate-x-36 hover:before:-translate-x-36 before:duration-500"
+          className="top-0 before:top-0 hover:-top-12 before:left-0 z-1 hover:z-11 before:absolute relative flex justify-center items-center before:bg-white/50 shadow-black/10 shadow-lg backdrop-blur-xs border border-white/40 border-r-white/20 border-b-white/20 rounded-xl w-20 before:w-12 h-20 before:h-full overflow-hidden text-white text-4xl no-underline before:content-[''] before:skew-x-45 before:transition before:translate-x-36 hover:before:-translate-x-36 before:duration-500"
         >
           <FaWhatsapp />
         </a>
