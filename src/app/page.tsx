891a57e2aa20bf277380d047efcf7bf1a87ee3a9
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-10-23 16:31:19 +0530

    animated theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import Main from "@/components/main/main";
+
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center min-h-screen font-alkatra text-5xl">
-      Glassmorphism UI
-    </div>
-  );
+  return <Main />;
 }
